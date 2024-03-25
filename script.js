949d0cb9f8541de5630a211c91638c73b4b883f3
KriztRuiz <156562708+KriztRuiz@users.noreply.github.com>
2024-03-25 17:27:14 -0600

    Update script.js
    primer cambio en el js
    25 de marzo del 2024
    solo comente la primera entrada

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.getElementById('colorButton').addEventListener('click', function() {
+/*document.getElementById('colorButton').addEventListener('click', function() {
     var button = document.getElementById('colorButton');
     var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
     button.style.backgroundColor = randomColor;
 });
+*/
